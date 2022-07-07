b0c6f0cbe9080699c46ab0fd3e24db5356e766c3
Viet Nguyen 168 <vietnguyen168@gmail.com>
2022-07-07 13:48:57 -0400

    CG-7 framework for timeline

--- Code/Src/main.cpp
+++ Code/Src/main.cpp
@@ -15,9 +15,11 @@
 #include "human_female.h"
 #include "idSystem.h"
 #include "iostream"
+#include "string.h"
 
 int main() {
   std::vector<human*> human_lst;
+  string end_pg = "end of  program";
   human_female* first_fm = new human_female("Lilith", 0);
   human_female* second_fm = new human_female("Eva", 0);
   human_lst.push_back(first_fm);
@@ -27,6 +29,8 @@ int main() {
     person->introduce();
     std::this_thread::sleep_for(chrono::milliseconds(1000));
   }
+
+  printf("%s \n", end_pg.c_str());
 
   return 0;
 }
